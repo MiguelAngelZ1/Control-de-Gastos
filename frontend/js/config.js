2b00925db465e83e1453052c381d02325a9bd14d
MiguelAngelZ1 <miguelanyelo0011@gmail.com>
2025-05-18 03:02:51 +0000

    Despliegue exitoso

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
 // Archivo: config.js
 // Cambia esta URL por la de tu backend en Render
-const API_BASE_URL = 'https://control-gastos-backend.onrender.com/api';
+const API_BASE_URL = 'https://control-de-gastos-z8c2.onrender.com/api';
