1f6c42d9de45b42fd1244dff6e393ad77f5ab7e1
MiguelAngelZ1 <miguelanyelo0011@gmail.com>
2025-05-23 15:33:40 +0000

    Problemas con backend

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 // Archivo: config.js
 // Cambia esta URL por la de tu backend en Render
 const API_BASE_URL = 'https://control-de-gastos-z8c2.onrender.com/api';
+window.API_BASE_URL = API_BASE_URL;
